388dbd46f1373bac3c9ec79cd8e8636384b9c23b
Morioo <107799693+Morioo@users.noreply.github.com>
2024-09-04 12:50:07 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ograniczenia
+{
+    public class DataStore<T> where T : struct
+    {
+
+        public T Data { get; set; }
+
+    }
+    public struct Mystr
+    {
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            DataStore<int> store = new DataStore<int>();
+            Console.WriteLine(store.Data);
+            DataStore<char> chstore = new DataStore<char>();
+            Console.WriteLine(chstore.Data);
+            DataStore<Mystr> mst = new DataStore<Mystr>();
+
+            //DataStore<string> str1 = new DataStore<string>();
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ kolekcje.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+
+
+
+    class Program
+    {
+    static void Main(string[] args)
+    {
+
+
+        List<int> numList = new List<int>();
+        numList.Add(1);
+        numList.Add(3);
+        numList.Add(5);
+        numList.Add(7);
+        List<string> strList = new List<string>();
+        strList.Add("str1");
+
+        var strListVar = new List<string>();
+        strListVar.Add("Jakis sobie string");
+
+        var BigCities = new List<string>()
+        {
+            "Warszawa",
+            "Kraków"
+
+
+        };
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    }
+   
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
